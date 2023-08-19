6c32901db9c24cfe297c8b8309a946f4def679f0
forrestmorrison <forrestmorrison@gmail.com>
2023-08-18 22:42:53 -0500

    created NavBar component, added basic layout CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom"
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost />} />
-      </Routes>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      </main>
     </div>
   );
 }
