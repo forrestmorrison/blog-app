45c0a58f1bedd2e96d5544a9047d6cbb4262e559
forrestmorrison <forrestmorrison@gmail.com>
2023-08-18 23:11:45 -0500

    added logo & links to NavBar with styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import Logo from "../images/my-blog-logo.png"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navbar">
+        <Link to="/"
+            style={{
+                height: "100%",
+                display: "flex",
+                alignItems: "center"
+            }}
+        >
+            <img src={Logo} alt="logo" className="logo" />
+        </Link>
+        <div className="links">
+            <Link to="/createpost"
+                style={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    color: "#0275d8",
+                    margin: "0 10px",
+                    fontWeight: "bold",
+                    textDecoration: "none"
+                }}
+            >
+                new post
+            </Link>
+            <Link to="/"
+                style={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    color: "#0275d8",
+                    margin: "0 10px",
+                    fontWeight: "bold",
+                    textDecoration: "none"
+                }}
+            >
+                log out
+            </Link>
+        </div>
+    </div>
   )
 }
 
