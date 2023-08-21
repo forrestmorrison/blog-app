529f6a3536cef9ef549467e5fee4492ab7363d40
forrestmorrison <forrestmorrison@gmail.com>
2023-08-20 22:04:24 -0500

    added useState, onChange & onSubmit functions to CreatePost

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,35 +1,58 @@
+import { useState } from "react"
 import { Button, TextField } from "@mui/material"
 
-const CreatePost = () => {
+const CreatePost = (props) => {
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const handleBodyChange = (e) => {
+    setBody(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="create-post">
       <h4>title</h4>
-      <TextField 
-        sx={{
-          width: "100%"
-        }}
-      />
-      <h4>body</h4>
-      <TextField 
-        sx={{
-          width: "100%"
-        }}
-        multiline
-        rows={12}
-      />
-      <Button
-        sx={{
-          marginTop: "10px",
-          fontWeight: "bold",
-          backgroundColor: "#0275d8",
-          color: "white",
-          "&:hover": {
-            color: "#0275d8"
-          }
-        }}
-      >
-        Submit
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <TextField 
+          sx={{
+            width: "100%"
+          }}
+          onChange={handleTitleChange}
+          value={title}
+        />
+        <h4>body</h4>
+        <TextField 
+          sx={{
+            width: "100%"
+          }}
+          multiline
+          rows={12}
+          onChange={handleBodyChange}
+          value={body}
+        />
+        <Button
+          sx={{
+            marginTop: "10px",
+            fontWeight: "bold",
+            backgroundColor: "#0275d8",
+            color: "white",
+            "&:hover": {
+              color: "#0275d8"
+            }
+          }}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
     </div>
   )
 }
