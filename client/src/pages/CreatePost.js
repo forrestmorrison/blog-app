fd3a6c7b81653f909e91b1552dbae7f877d89361
forrestmorrison <forrestmorrison@gmail.com>
2023-08-20 20:14:52 -0500

    added TextFields & Button to CreatePost page with styling

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="create-post">
+      <h4>title</h4>
+      <TextField 
+        sx={{
+          width: "100%"
+        }}
+      />
+      <h4>body</h4>
+      <TextField 
+        sx={{
+          width: "100%"
+        }}
+        multiline
+        rows={12}
+      />
+      <Button
+        sx={{
+          marginTop: "10px",
+          fontWeight: "bold",
+          backgroundColor: "#0275d8",
+          color: "white",
+          "&:hover": {
+            color: "#0275d8"
+          }
+        }}
+      >
+        Submit
+      </Button>
+    </div>
   )
 }
 
